92b18a587c709b04b87eeed938cab9ccebb7b624
Oliver Bullock <ojbullock@googlemail.com>
2024-10-27 00:04:38 +0100

    Updated to setup an API - controllers need sorting out as doesn't build.

---
+++ source/kinetic-coder.personalfinance.app.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MySolution.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    Summaries[Random.Shared.Next(Summaries.Length)]
+                ))
+                .ToArray();
+            return forecast;
+        }
+    }
+
+    public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary);
+}
